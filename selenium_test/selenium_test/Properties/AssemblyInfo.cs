85ba6a07ee813eac14b47e7f843a89ffdfe088d4
shaad-pc-win10 <shaad-pc-win10@DESKTOP-VET32K8>
2018-04-07 13:42:51 -0700

    commit all

---
+++ selenium_test/selenium_test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("selenium_test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("selenium_test")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("b8564457-3cc9-49fb-a1f1-41dc8a1f3d63")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ selenium_test/selenium_test/UnitTest1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.IE;
+using OpenQA.Selenium.PhantomJS;
+using OpenQA.Selenium.Remote;
+
+namespace selenium_test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        private string baseURL = "http://www.google.com/";
+        private RemoteWebDriver driver;
+        private string browser = string.Empty;
+        public TestContext TestContext { get; set; }
+
+        [TestMethod]
+        [TestCategory("Selenium")]
+        [Priority(1)]
+        [Owner("FireFox")]
+        public void TestMethod1()
+        {
+            driver.Manage().Window.Maximize();
+            driver.Manage().Timeouts().ImplicitlyWait(TimeSpan.FromSeconds(30));
+            driver.Navigate().GoToUrl(this.baseURL);
+
+            //do other Selenium things here!
+        }
+
+        [TestCleanup()]
+        public void MyTestCleanup()
+        {
+            driver.Quit();
+        }
+
+        [TestInitialize()]
+        public void MyTestInitialize()
+        {
+            //Set the browswer from a build
+            browser = this.TestContext.Properties["browser"] != null ? this.TestContext.Properties["browser"].ToString() : "chrome";
+            switch (browser)
+            {
+                case "firefox":
+                    driver = new FirefoxDriver();
+                    break;
+                case "chrome":
+                    driver = new ChromeDriver();
+                    break;
+                case "ie":
+                    driver = new InternetExplorerDriver();
+                    break;
+                case "PhantomJS":
+                    driver = new PhantomJSDriver();
+                    break;
+                default:
+                    driver = new PhantomJSDriver();
+                    break;
+            }
+            if (this.TestContext.Properties["Url"] != null) //Set URL from a build
+            {
+                this.baseURL = this.TestContext.Properties["Url"].ToString();
+            }
+            else
+            {
+                this.baseURL = "http://www.google.com/"; //default URL just to get started with
+            }
+        }
+    }
+}
+        
+
+
+    
